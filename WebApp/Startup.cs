7b79f78b33779aa8168af31641d8e1a348a7ce69
Volodymyr Zahoretskyi <zagoretskiy.vova@ukr.net>
2022-07-02 14:48:27 +0300

    Remove unnecessary elements

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Plugins.DataStore.InMemory;
 using Plugins.DataStore.SQL;
 using System;
 using System.Collections.Generic;
